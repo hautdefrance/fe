b502efc74a852f7d85f33caf201c667354f11127
ulivz <472590061@qq.com>
2018-06-21 01:02:12 +0800

    bump vuepress

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:ulivz/awesome-front-end.git master:gh-pages
+git push -f https://github.com/ulivz/awesome-front-end.git master:gh-pages
 
 cd -
