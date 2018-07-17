0be3f0c36a673c3bfa4325da9ccc633b20d0ef06
ULIVZ <472590061@qq.com>
2018-07-17 08:37:27 +0800

    feat: give card more properties

--- docs/.vuepress/components/card.vue
+++ docs/.vuepress/components/card.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="card" :class="{ 'shadow': shadow }">
+  <div class="card"
+       :class="{
+          'shadow': shadow,
+          'no-padding': noPadding,
+          'flex': flex,
+       }">
     <slot></slot>
   </div>
 </template>
@@ -10,6 +15,14 @@
       shadow: {
         type: Boolean,
         default: false
+      },
+      noPadding: {
+        type: Boolean,
+        default: false
+      },
+      flex: {
+        type: Boolean,
+        default: false
       }
     }
   }
@@ -17,15 +30,20 @@
 
 <style lang="stylus" scoped>
   .card {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-top: 10px;
+    margin: 15px 0;
     padding: 10px;
+    &.flex {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
     &.shadow {
       border: 1px solid #f9f9f9;
       border-radius: 6px;
       box-shadow: -1px 1px 1px 0 rgba(0, 0, 0, .05), 0 10px 20px 0 rgba(0, 0, 0, .15);
+    }
+    &.no-padding {
+      padding: 0px;
     }
   }
 </style>
