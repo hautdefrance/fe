115ba2cfd3894121e5415daec7076aafad88046d
ULIVZ <472590061@qq.com>
2018-08-13 12:16:35 +0800

    docs: init float 布局

---
+++ docs/.vuepress/components/css-layout/float-2.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="css-layout-float-2">
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+  .css-layout-float-2 {
+    @extend $clearfix;
+  }
+
+  .item {
+    float: left;
+    width: 32%;
+    height: 40px;
+    background-color: #f2f2f2;
+    margin-right: 2%;
+    &:last-child {
+      margin-right: 0;
+    }
+    // 3x + 2y = 100
+    // y = 2
+  }
+</style>
