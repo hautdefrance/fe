3395ad20be68f3ee480b6abd7bdc791940152c9c
ULIVZ <472590061@qq.com>
2018-08-14 23:50:15 +0800

    docs: refine the white-space

--- docs/.vuepress/components/css/white-space.vue
+++ docs/.vuepress/components/css/white-space.vue
@@ -1,37 +1,67 @@
 <template>
   <div class="css-white-space">
-    <ul>
-      <li>
-        <b>white-space: normal;</b>
-        <div class="block block-1">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </li>
-      <li>
-        <b>white-space: nowrap;</b>
-        <div class="block block-2">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </li>
-      <li>
-        <b>white-space: pre;</b>
-        <div class="block block-3">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </li>
-    </ul>
+    <div>
+      <h4>2.2.1 white-space: normal</h4>
+      <ul>
+        <li>
+          <b>示例：</b>
+          <div class="block block-1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum.
+          </div>
+        </li>
+        <li>
+          <p><b>特点</b>：DOM 解析时忽略所有空白，超过部分会换行。</p>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h4><b>2.2.2 white-space: nowrap;</b></h4>
+      <ul>
+        <li>
+          <b>示例：</b>
+          <div class="block block-2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum.
+          </div>
+        </li>
+        <li>
+          <p><b>特点</b>：DOM 解析时忽略所有空白，超过部分不会换行。</p>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h4><b>2.2.3 white-space: pre;</b></h4>
+      <ul>
+        <li>
+          <b>示例：</b>
+          <div class="block block-3">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum.
+          </div>
+        <li>
+          <p><b>特点</b>：DOM 解析时保留所有缩进、换行和空格。和 <code>pre</code> 的工作机制相似。</p></li>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
